14a3f922e04d3d081988fba25ccab0613ec865a1
karloseq <87254977+ahmar7@users.noreply.github.com>
2023-04-29 02:37:04 +0500

    mbl animation added

--- js/index.js
+++ js/index.js
@@ -1,32 +1,49 @@
-$(document).ready(function() {
-  
-    $('.menu-icon-header').on('click', function() {
-      $('.menu-top').toggleClass("slide-in");
-      $('.menu-bottom').toggleClass("slide-in");
-      
+$(document).ready(function () {
+  $(".menu-icon-header").on("click", function () {
+    $(".menu-top").toggleClass("slide-in");
+    $(".menu-bottom").toggleClass("slide-in");
+
     //   if ($(this).text() == "Menu") {
-    //    $(this).text("Close"); 
+    //    $(this).text("Close");
     //   }
     //   else {
-    //     $(this).text("Menu"); 
+    //     $(this).text("Menu");
     //   }
-    });
-    
   });
-  let splashScreen =()=>{
-    let preloader = document.getElementById("preloader");
-    let bodySelect = document.querySelector('body')
-    preloader.style.display="none"
-    bodySelect.style.overflow="auto"
-  }
-  var animation = bodymovin.loadAnimation({
-    container: document.getElementById("lottie"), // Required
-    path: "../animation/BB_Revision_Visible.json", // Required
-    renderer: "svg", // Required
-    loop: false, // Optional
-    autoplay: true, // Optional
-    name: "Hello World", // Name for future reference. Optional.
-  });
+});
+let splashScreen = () => {
+  let preloader = document.getElementById("preloaderdesk");
+  let bodySelect = document.querySelector("body");
+  preloader.classList.add("fading-splash");
+  bodySelect.style.overflow = "auto";
+};
+let splashScreenmbl = () => {
+  let preloader = document.getElementById("preloadermbl");
+  let bodySelect = document.querySelector("body");
+  preloader.classList.add("fading-splash");
+  bodySelect.style.overflow = "auto";
+};
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("lottie"), 
+  path: "../animation/BB_Revision_Visible.json", 
+  renderer: "svg", 
+  loop: true, 
+  autoplay: true, 
+});
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("lottiembl"), 
+  path: "../animation/lottie_animations_2.json", 
+  renderer: "svg", 
+  loop: false, 
+  autoplay: true, 
+});
+setTimeout(() => {
+  splashScreen();
+  splashScreenmbl();
   setTimeout(() => {
-    splashScreen()
-  }, 3000);
+    let preloader = document.getElementById("preloaderdesk");
+    let preloadermbl = document.getElementById("preloadermbl");
+    preloader.style.display = "none";
+    preloadermbl.style.display = "none";
+  }, 500);
+}, 3500);
